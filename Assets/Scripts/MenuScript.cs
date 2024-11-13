0f2c7c1a1345bf107252840e61e193accdfed746
Asha <as435977@ucf.edu>
2024-11-13 14:14:18 -0500

    Menu Updates

---
+++ Assets/Scripts/MenuScript.cs
@@ -0,0 +1,63 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+
+public class MenuScript : MonoBehaviour 
+{
+    public GameObject MM;
+    public GameObject MG;
+    public GameObject C;
+
+    public void PlayGame()
+    {
+        SceneManager.LoadScene(1);
+    }
+
+    public void MiniGames()
+    {
+        MG.SetActive(true);
+        MM.SetActive(false);
+        C.SetActive(false);
+    }
+
+    public void Controls()
+    {
+        MG.SetActive(false);
+        MM.SetActive(false);
+        C.SetActive(true);
+    }
+
+    public void Back()
+    {
+        if (MG.activeSelf == true)
+        {
+            MG.SetActive(false);
+            MM.SetActive(true);
+            C.SetActive(false);
+        }
+        else if (C.activeSelf == true)
+        {
+            MG.SetActive(false);
+            MM.SetActive(true);
+            C.SetActive(false);
+        }
+    }
+
+    // MiniGames
+    public void DancingSeed()
+    {
+        SceneManager.LoadScene(1);
+    }
+
+    public void PTI()
+    {
+        SceneManager.LoadScene(2);
+    }
+
+    public void Quit()
+    {
+        Application.Quit();
+    }
+}
