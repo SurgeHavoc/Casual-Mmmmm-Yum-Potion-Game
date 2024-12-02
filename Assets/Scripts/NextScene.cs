f8ab2b7e69afab3dbea8a54860e0f0e5915f1adb
SurgeHavoc <87490063+SurgeHavoc@users.noreply.github.com>
2024-12-02 00:32:12 -0500

    Another Quick Update
    - Changed the YouWinScene to make it proceed to a random minigame.

--- Assets/Scripts/NextScene.cs
+++ Assets/Scripts/NextScene.cs
@@ -1,31 +1,49 @@
 using UnityEngine;
 using System.Collections;
 using UnityEngine.SceneManagement;
+using System.Collections.Generic;
 
 public class NextScene : MonoBehaviour
 {
 
     private void Start()
     {
-        StartCoroutine(LoadNextLevelAfterDelay(3f));
-    }
-    IEnumerator LoadNextLevelAfterDelay(float delay)
-    {
-        yield return new WaitForSeconds(delay);
-        LoadNextLevel();
+        Invoke("ProceedToNextMinigame", 3f);
     }
 
-    void LoadNextLevel()
+    void ProceedToNextMinigame()
     {
-        int NextSceneIndex = SceneManager.GetActiveScene().buildIndex + 1;
+        Debug.Log("Proceeding to a random minigame.");
 
-        if (NextSceneIndex < SceneManager.sceneCountInBuildSettings)
+        int MinSceneIndex = 7;
+        int MaxSceneIndex = 11;
+        int CurrentSceneIndex = SceneManager.GetActiveScene().buildIndex;
+
+        // Check to make sure that MaxSceneIndex doesn't exceed the number of scenes in build settings.
+        MaxSceneIndex = Mathf.Min(MaxSceneIndex, SceneManager.sceneCountInBuildSettings - 1);
+
+        // Create a list of possible scene indices excluding the current scene to avoid repeating the same scene for next minigame.
+        List<int> PossibleSceneIndices = new List<int>();
+        for (int i = MinSceneIndex; i <= MaxSceneIndex; i++)
         {
-            SceneManager.LoadScene(NextSceneIndex);
+            if (i != CurrentSceneIndex)
+            {
+                PossibleSceneIndices.Add(i);
+            }
+        }
+
+        if (PossibleSceneIndices.Count > 0)
+        {
+            // Select a random index from the list of possible scene indices.
+            int RandomIndex = Random.Range(0, PossibleSceneIndices.Count);
+            int RandomSceneIndex = PossibleSceneIndices[RandomIndex];
+
+            // Load the randomly selected scene other than the current scene.
+            SceneManager.LoadScene(RandomSceneIndex);
         }
         else
         {
-            Debug.Log("No more scenes to load!");
+            Debug.LogWarning("No other scenes available to load, try adding scenes to the build settings.");
         }
     }
 }
